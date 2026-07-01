754a0dcc2abb5b5c645554651ee784c1fb465166
Helen Machado <helengraci.mach@gmail.com>
2026-06-30 22:47:55 -0300

    Atualizando ano para query de dias sem chuva no hidroapp_statistics

--- app_boletim_diario.py
+++ app_boletim_diario.py
@@ -4592,13 +4592,15 @@ async def slide1_seca():
             """,
             unsafe_allow_html=True)
 
+        ano = datetime.now().year
+
         query_dias_sem_chuva = f"""select 
                                     c.cod_ibge,
                                     c."name",
                                     SUM(hs.dsc) AS dsc
                                 from hidroapp_statistics hs 
                                 left join cities c on c.id = hs.model_id
-                                where date_hour between '2025-04-01 03:00:00.000' and '2025-09-30 03:00:00.000'and model_type ='City'
+                                where date_hour between '{ano}-04-01 03:00:00.000' and '{ano}-09-30 03:00:00.000'and model_type ='City'
                                 group by c.cod_ibge, c."name";"""
         
         tabela_dsc_cities= execute_query(query_dias_sem_chuva)
